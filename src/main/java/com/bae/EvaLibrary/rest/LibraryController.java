77835e4e8d4a5bae8871f2d103cd46d5e7377781
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-07-24 20:13:09 +0100

    ELB-9 #comment Added Delete Functionality

--- src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
+++ src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
@@ -3,6 +3,8 @@ package com.bae.EvaLibrary.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,13 @@ public class LibraryController {
 	public void addBoook(@RequestBody Book book) {
 		System.out.println(book);
 		this.Library.add(book);
+	}
+
+	@DeleteMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable int id) {
+		this.Library.remove(id);
+
+		return "You have borrowed book number: " + id;
 	}
 
 }
