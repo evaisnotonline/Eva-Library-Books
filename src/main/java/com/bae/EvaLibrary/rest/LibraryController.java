c9afa08250707b6be6912827e8af89c355f2d177
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-07-24 22:32:14 +0100

    ELB-15 #comment Added H2 Database Functionality

--- src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
+++ src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
@@ -1,8 +1,10 @@
 package com.bae.EvaLibrary.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,38 +14,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bae.EvaLibrary.data.Book;
+import com.bae.EvaLibrary.service.BookService;
 
 @RestController
+@CrossOrigin
 public class LibraryController {
 
-	private List<Book> Library = new ArrayList<>();
+	private BookService service;
 
-	@PostMapping("/addBook")
-	public void addBoook(@RequestBody Book book) {
-		System.out.println(book);
-		this.Library.add(book);
+	public LibraryController(BookService service) {
+		super();
+		this.service = service;
 	}
 
-	@DeleteMapping("/deleteBook/{id}")
-	public String deleteBook(@PathVariable int id) {
-		this.Library.remove(id);
-
-		return "You have borrowed book number: " + id;
+	@PostMapping("/addBook")
+	public ResponseEntity<Book> createBook(@RequestBody Book book) {
+		Book created = this.service.createBook(book);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/getAllBooks")
 	public List<Book> getAllBooks() {
-		return this.Library;
-	}
-
-	@PutMapping("/replaceBook/{id}")
-	public Book replaceBook(@PathVariable int id, @RequestBody Book newBook) {
-		return this.Library.set(id, newBook);
+		return this.service.getAllBooks();
 	}
 
 	@GetMapping("/getBook/{id}")
 	public Book getBook(@PathVariable int id) {
-		Book found = this.Library.get(id);
+		Book found = this.service.getBook(id);
 		return found;
 	}
+
+	@PutMapping("/updateBook/{id}")
+	public Book updateBook(@PathVariable int id, @RequestBody Book newBook) {
+		return this.service.updateBook(id, newBook);
+	}
+
+	@DeleteMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable int id) {
+		return this.service.deleteBook(id);
+	}
+
 }
