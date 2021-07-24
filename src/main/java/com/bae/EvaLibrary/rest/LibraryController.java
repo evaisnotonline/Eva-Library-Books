2ba74bdf994578dc7600fe0e0e519ffddb2f3426
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-07-24 21:40:27 +0100

    ELB-8 #Added Update Functionality

--- src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
+++ src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,11 @@ public class LibraryController {
 	@GetMapping("/getAllBooks")
 	public List<Book> getAllBooks() {
 		return this.Library;
+	}
+
+	@PutMapping("/replaceBook/{id}")
+	public Book replaceBook(@PathVariable int id, @RequestBody Book newBook) {
+		return this.Library.set(id, newBook); // replace the kitten at index id
 	}
 
 }
