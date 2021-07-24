96e26415c5899c9ef385bf891361681fce3fe746
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-07-24 21:37:47 +0100

    ELB-11 #comment Added View All Functionality  git commit -m ELB-11

--- src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
+++ src/main/java/com/bae/EvaLibrary/rest/LibraryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,11 @@ public class LibraryController {
 		this.Library.remove(id);
 
 		return "You have borrowed book number: " + id;
+	}
+
+	@GetMapping("/getAllBooks")
+	public List<Book> getAllBooks() {
+		return this.Library;
 	}
 
 }
